e7db45cae0b7564d7f8b0ae156cc7d027c44b03c
Jihoon Kim <kjh618@kaist.ac.kr>
2021-06-03 18:01:36 +0900

    Setup cross compile using cross

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cargo build --target=arm-unknown-linux-gnueabi && scp target/arm-unknown-linux-gnueabi/debug/bluetooth2usb pi@raspberrypi.local:~
+# sudo dockerd &
+
+RUSTFLAGS='-L /usr/arm-linux-gnueabi/lib/ -L /usr/lib/arm-linux-gnueabi/' \
+    cross build --target arm-unknown-linux-gnueabi && \
+    scp target/arm-unknown-linux-gnueabi/debug/bluetooth2usb pi@raspberrypi.local:~
