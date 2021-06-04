21051e8d33e4a3fde3463c0716e652ab848c4eb7
Jihoon Kim <kjh618@kaist.ac.kr>
2021-06-04 14:14:07 +0900

    Change to Python

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM rustembedded/cross:arm-unknown-linux-gnueabi-0.2.1
-
-# Install libdbus libraries and pkg-config
-RUN dpkg --add-architecture armhf && \
-    apt-get update && \
-    apt-get install --assume-yes libdbus-1-dev libdbus-1-dev:armhf pkg-config
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# sudo dockerd &
-
-RUSTFLAGS='-L /usr/arm-linux-gnueabi/lib/ -L /usr/lib/arm-linux-gnueabi/' \
-    cross build --target arm-unknown-linux-gnueabi && \
-    scp target/arm-unknown-linux-gnueabi/debug/bluetooth2usb pi@raspberrypi.local:~
